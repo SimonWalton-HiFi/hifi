2351b25882e4d5837ae4811690f74e8200259e62
Simon Walton <simon@highfidelity.io>
2018-04-09 13:21:03 -0700

    Merge Clement's PR12700 diffs for early trace start

--- libraries/qml/src/qml/impl/SharedObject.cpp
+++ libraries/qml/src/qml/impl/SharedObject.cpp
@@ -106,7 +106,10 @@ void SharedObject::create(OffscreenSurface* surface) {
 
     // Create a QML engine.
     auto qmlEngine = acquireEngine(surface);
-    _qmlContext = new QQmlContext(qmlEngine->rootContext(), qmlEngine);
+    {
+        PROFILE_RANGE(startup, "new QQmlContext");
+        _qmlContext = new QQmlContext(qmlEngine->rootContext(), qmlEngine);
+    }
     surface->onRootContextCreated(_qmlContext);
     emit surface->rootContextCreated(_qmlContext);
 
@@ -176,6 +179,7 @@ static size_t globalEngineRefCount{ 0 };
 #endif
 
 QQmlEngine* SharedObject::acquireEngine(OffscreenSurface* surface) {
+    PROFILE_RANGE(startup, "acquireEngine");
     Q_ASSERT(QThread::currentThread() == qApp->thread());
 
     QQmlEngine* result = nullptr;
