4b0afd9cb10c8ee8edd03fc23d340843309265f4
Simon Walton <simon@highfidelity.io>
2019-11-13 10:26:53 -0800

    Fixes for Dockerfile; check-in config script I've been using

---
+++ .github/workflows/actions/actions_config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export CI_BUILD=Github
+export BUILD_TYPE=Release
+export RELEASE_TYPE=PR
+export RELEASE_NUMBER=99
+export VERSION_CODE=99
+export GIT_PR_COMMIT=feedbeef
+export GIT_PR_COMMIT_SHORT=feedbeef
+export HIFI_VCPKG_BOOTSTRAP=true
+export GA_TRACKING_ID=UA-39558647-8
+export PreferredToolArchitecture=X64
+export OCULUS_APP_ID=1255907384473836
+
+cmake /home/simon/src/hifi -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TOOLS:BOOLEAN=FALSE
